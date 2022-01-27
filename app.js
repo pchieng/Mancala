f314d6976e1a017a78138dadb5d8469950326176
Peter Chieng <pchieng3@gmail.com>
2022-01-26 19:47:26 -0600

    Initial board setup

---
+++ app.js
@@ -0,0 +1,35 @@
+// const mancalaBoard = document.getElementById("mancalaBoard");
+
+// const func = function () {
+//   console.log("Board Clicked")
+// }
+
+// mancalaBoard.addEventListener("click", func)
+
+// let currentOperation = () => {};
+
+// mancalaBoard.addEventListener("click", () => {
+//   currentOperation = function (a,b) {
+
+//   }
+// })
+
+
+// setinterval(tick, 1000)
+
+
+
+const board = [
+    4, 4, 4, 4, 4, 4, 0, /* player 1 */ 4, 4, 4, 4, 4, 4, 0 /* player 2 */
+  ];
+
+
+  const gameState = {
+    board: board, // from above
+    currentPlayer: 0 // switch to 1 when the player swaps
+  };
+
+
+
+
+
